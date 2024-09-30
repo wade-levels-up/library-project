77b7254f13a6dfa6c594f29f15c777b1978fb95d
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-09-30 20:21:32 +1000

    Refactor Book contructor into class
    Project still runs as it did before, just changed
    Book constructor into Book class.

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const tbody = document.querySelector('tbody');
 
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 // Add initial books to library
@@ -29,6 +31,7 @@ function updateReadStatus(index) {
     }
     populateTable(myLibrary);
 }
+
 
 function populateTable(array) {
 
